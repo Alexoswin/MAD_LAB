12251be0d641c870be039f5a0a42041526999c4d
oswin <oswinalex1@gmail.com>
2024-04-01 23:40:54 +0530

    map

---
+++ dashbord/lib/main.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart' show rootBundle;
+import 'package:flutter_map/flutter_map.dart';
+
+// lat long
+import 'package:latlong2/latlong.dart';
+
+void main() => runApp(MaterialApp(
+  home: Home(),
+));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Nasha mukti"),
+        backgroundColor: Colors.blue,
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          Card(
+            // Increased padding to 50.0
+            child: Image(image: AssetImage('images/i1.png')),
+          ),
+          Expanded(
+
+            child: content(),
+         // Show the map here
+          ),
+
+        ],
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton.extended(
+            onPressed: () {},
+            label: Text("Book an appointment"),
+            backgroundColor: Colors.blue,
+          ),
+          SizedBox(height: 10), // Add some space between the two buttons
+          FloatingActionButton.extended(
+            onPressed: () {
+              // Handle button press
+            },
+            label: Text("Register as patient"),
+            backgroundColor: Colors.blue,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget content() {
+    height : 300;
+    width: 300;
+    return FlutterMap(
+      options: MapOptions(
+        initialCenter: LatLng(19.0760, 72.8777),
+
+        initialZoom: 10.2,
+        interactionOptions: const InteractionOptions(flags: ~InteractiveFlag.doubleTapZoom),
+      ),
+      children: [
+          openStreetMapTileLayer,
+      ],
+    );
+  }
+}
+TileLayer get openStreetMapTileLayer => TileLayer(
+urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+userAgentPackageName: 'dev.fleaflet.flutter_map.example',
+);
